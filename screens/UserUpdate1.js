ce8eb281efdc0d4e67cbeb3ae1c4fa296e54178c
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-04-28 12:23:43 +0100

    comment added

--- screens/UserUpdate1.js
+++ screens/UserUpdate1.js
@@ -1,38 +1,48 @@
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
+import {  StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
-import { getAuth, updateEmail,updatePassword, updateProfile, createUserWithEmailAndPassword ,signInWithEmailAndPassword , onAuthStateChanged} from "firebase/auth";
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { getStorage, ref, s, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getAuth, updateEmail,updatePassword, updateProfile} from "firebase/auth";
+import { useRoute } from '@react-navigation/native';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import * as ImagePicker from 'expo-image-picker';
 import { useSelector } from 'react-redux';
-import { selectUid , selectAmail} from '../components/authSlice';
+import { selectUid } from '../components/authSlice';
 import { db, } from '../firebase';
-import { collection, query, where, getDocs, updateDoc, Timestamp, doc, serverTimestamp, setDoc , getFirestore} from 'firebase/firestore';
+import { collection, query, where, getDocs,  setDoc } from 'firebase/firestore';
 
 
+// Define UserUpdate component
 const UserUpdate = ({navigation}) => {
-    const route = useRoute();
-      const { item1, item2 , item4} = route.params;
-         const [isLoading, setIsLoading] = useState(false);
+  
+   // Get params from route
+  const route = useRoute();
+  const { item1, item2 , item4 } = route.params;
+
+  // Define state variables
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState(item1);
-  const[error, setError] = useState("")
-  const[success, setSuccess] = useState("")
-  const [password,  setPassword] = useState("")
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [password,  setPassword] = useState("");
   const [username, setUsername] = useState(item2);
   const [photo, setPhoto] = useState('');
   const [hoto, setHoto] = useState(item4);
-   const [picture, setPicture] = useState(item4);
+  const [picture, setPicture] = useState(item4);
   const [downloadURL, setDownloadURL] = useState(null);
   const [file, setFile] = useState(null);
-    const [loading, setLoading] = useState(false);
-      const [prog, setProg] = useState(false);
-  const uid = useSelector(selectUid);
-const auth = getAuth();
-const storage = getStorage();
+  const [loading, setLoading] = useState(false);
+  const [prog, setProg] = useState(false);
 
+  // Get user ID from Redux store
+  const uid = useSelector(selectUid)
 
-const pickImage = async () => {
+  
+// Get Firebase auth and storage instances
+  const auth = getAuth();
+  const storage = getStorage();
+
+// Define function to pick an image from device's gallery
+  const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -46,62 +56,67 @@ const pickImage = async () => {
     }
   };
 
-useEffect(()=>{
-   const uploadFile = async() =>{
-     const date = new Date();
-    const name =  photo;
-     const storageRef = ref(storage, "images/" + date.toLocaleString() + name.slice(137,260));
-    const response = await fetch(photo);
-const blob = await response.blob();
-const uploadTask = uploadBytesResumable(storageRef, blob);
-uploadTask.on('state_changed', 
-  (snapshot) => {
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    
-    console.log('Upload is ' + progress +'% done');
-    setProg(progress)
-    setFile('Upload is ' + progress +'% done')
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-        default:
-          break;
+  // Upload image to Firebase storage when `photo` state changes
+  useEffect(()=>{
+    const uploadFile = async() =>{
+      const date = new Date();
+      const name =  photo;
+      const storageRef = ref(storage, "images/" + date.toLocaleString() + name.slice(137,260));
+      const response = await fetch(photo);
+      const blob = await response.blob();
+      const uploadTask = uploadBytesResumable(storageRef, blob);
+      
+      // Update progress bar
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress +'% done');
+          setProg(progress);
+          setFile('Upload is ' + progress +'% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            default:
+              break;
+          }
+        }, 
+        (error) => {
+          console.log(error);
+        }, 
+        () => {
+          // Get download URL of uploaded image
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            setDownloadURL(downloadURL);
+            setHoto(downloadURL);
+          });
+        }
+      );
     }
-  }, 
-  (error) => {
-    console.log(error);
-  }, 
-  () => {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
-     setDownloadURL(downloadURL);
-     setHoto(downloadURL);
-     set
-    });
-  }
-);
-   }
-   if (photo) {
-    uploadFile();
-  }
-},[photo] )
+    if (photo) {
+      uploadFile();
+    }
+  },[photo] )
 
+// function to update user's email, password, displayName and photoURL in Firebase Authentication
 const updateEmailAndPassword = async (uid, newEmail, newPassword, displayName, photoURL) => {
   try {
     const user = await auth.currentUser;
     
+    // check if uid is provided
     if (uid) {
    
+      // update email and password
       await updateEmail(user, newEmail);
       await updatePassword(user, newPassword);
       console.log('Email and password updated successfully');
-       await updateProfile(user, { displayName, photoURL})  
+       
+      // update display name and photo URL
+      await updateProfile(user, { displayName, photoURL})  
       console.log("photo"+ photoURL)
       setSuccess(null);
     }
@@ -110,10 +125,12 @@ const updateEmailAndPassword = async (uid, newEmail, newPassword, displayName, p
   }
 };
 
+// function to update user's data in Firestore
 const handleData = async (e) => { 
   const q = await query(collection(db, "cities"), where("id", "==", uid));
   const querySnapshot = await getDocs(q);
 
+  // loop through all documents returned by the query and update their data
   for (const doc of querySnapshot.docs) {
     const newData = {
       ...doc.data(),
@@ -124,33 +141,37 @@ const handleData = async (e) => {
   }
 };
 
+// set initial state for isSuccess to false
+const [isSuccess, setIsSuccess] = useState(false);
 
-
- const [isSuccess, setIsSuccess] = useState(false);
-  const handleSave = async () => {
-      if (prog !== 100 && hoto == null) {
+// function to handle form submit and update user's email, password, displayName and photoURL in Firebase Authentication and Firestore
+const handleSave = async () => {
+  // check if image upload is not yet 100% and hoto is null
+  if (prog !== 100 && hoto == null) {
     setError("wait for image upload to be 100%");
     setSuccess(null)
   }
   setLoading(true);
-    setIsLoading(true);
+  setIsLoading(true);
     
-    try {
-      await updateEmailAndPassword(uid, email, password, username, downloadURL);
-      await handleData();
-      // handle success
-    } catch (error) {
-      // handle error
-    } finally {
-      setIsSuccess(true);
-      setIsLoading(false);
-      setLoading(false);
-      setError(null)
-      setSuccess("Succesfully updated")
-    }
-  };
+  try {
+    // update email, password, displayName and photoURL in Firebase Authentication
+    await updateEmailAndPassword(uid, email, password, username, downloadURL);
+    
+    // update user's data in Firestore
+    await handleData();
 
-
+    // handle success
+  } catch (error) {
+    // handle error
+  } finally {
+    setIsSuccess(true);
+    setIsLoading(false);
+    setLoading(false);
+    setError(null)
+    setSuccess("Succesfully updated")
+  }
+};
 
    return (
          <LinearGradient colors={['#420C58',  '#211134', '#594677']} 
@@ -217,14 +238,8 @@ const handleData = async (e) => {
         <Text style={styles.buttonText}>Save</Text>
         </TouchableOpacity> 
        </View>
-
-  
       </View>
-
      </LinearGradient>
-
-     
-    
   )
 }
 
