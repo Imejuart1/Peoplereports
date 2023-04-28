6ce19ecdccd9d341afb15f175664e47a052acf4e
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-04-28 14:35:15 +0100

    hide password

--- firebase.js
+++ firebase.js
@@ -3,18 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from 'react-native-dotenv';
+
+
 
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
   // ...
-    apiKey: "AIzaSyDHp5maA9CFj4UkXe_xzlEiRtt0HxKfHss",
-  authDomain: "pipureport-6b980.firebaseapp.com",
-  projectId: "pipureport-6b980",
-  storageBucket: "pipureport-6b980.appspot.com",
-  messagingSenderId: "246680606543",
-  appId: "1:246680606543:web:d518f2534cbfd5b45a173a"
+   apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 // Initialize Firebase
